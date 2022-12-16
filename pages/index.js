9f406952bfea01984d1a62a3a65d9ff82ce94bbb
taty2010 <shydalys123@gmail.com>
2022-12-16 15:33:25 -0600

    added api back in

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Header from "@components/Header";
 import Footer from "@components/Footer";
 import FeedbackForm from "@components/FeedbackForm";
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <div className="container">
       <Head>
@@ -17,6 +17,7 @@ export default function Home() {
         <p className="description">
           Here's an example of a Netlify Form! When you fill this out, the
           submissions can be found in the Netlify Admin site. 
+          {data.name}
         </p>
         {/* <FeedbackForm name={data.name}/> */}
       </main>
@@ -25,10 +26,10 @@ export default function Home() {
   );
 }
 
-// export async function getServerSideProps() {
-//   // Fetch data from external API
-//   const res = await fetch(process.env.NETLIFY == "true" ? `${process.env.URL}/api/joke` : process.env.CONTEXT == "deploy-preview" ? `${process.env.DEPLOY_PRIME_URL}/api/joke` : 'http://localhost:8888/api/joke')
-//   const data = await res.json()
-//   // Pass data to the page via props
-//   return { props: { data } }
-// }
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(process.env.CONTEXT == "production" ? `${process.env.URL}/api/joke` : process.env.CONTEXT == "deploy-preview" ? `${process.env.DEPLOY_PRIME_URL}/api/joke` : 'http://localhost:8888/api/joke')
+  const data = await res.json()
+  // Pass data to the page via props
+  return { props: { data } }
+}
