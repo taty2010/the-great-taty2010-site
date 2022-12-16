a9d281aad5fa7e19c7a76f75c83f66aeec7b1f50
taty2010 <shydalys123@gmail.com>
2022-12-16 16:07:17 -0600

    eh

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ import Footer from "@components/Footer";
 import { useEffect, useState } from "react";
 import FeedbackForm from "@components/FeedbackForm";
 
-export default function Home({ res }) {
+export default function Home({ data }) {
   const[ name, setName ] = useState("")
   useEffect(() => {
-    setName(res.name)
+    setName(data.name)
   }, []);
 
   return (
@@ -23,7 +23,7 @@ export default function Home({ res }) {
         <p className="description">
           Here's an example of a Netlify Form! When you fill this out, the
           submissions can be found in the Netlify Admin site. 
-          {name}
+          {name && name}
         </p>
         {/* <FeedbackForm name={data.name}/> */}
       </main>
@@ -34,10 +34,10 @@ export default function Home({ res }) {
 
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch(process.env.CONTEXT == "production" ? `${process.env.URL}/api/joke` : process.env.CONTEXT == "deploy-preview" ? `${process.env.DEPLOY_PRIME_URL}/api/joke` : 'http://localhost:8888/api/joke')
+  const data = await fetch(process.env.CONTEXT == "production" ? `${process.env.URL}/api/joke` : process.env.CONTEXT == "deploy-preview" ? `${process.env.DEPLOY_PRIME_URL}/api/joke` : 'http://localhost:8888/api/joke')
   .then(res => res.json())
   .catch(err => console.log(err))
 
   // Pass data to the page via props
-  return { props: { res } }
+  return { props: { data } }
 }
